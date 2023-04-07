023ca05f76739fb21271dd14a5cce52dfded617c
Minh Kỳ <minhky.book@gmail.com>
2023-04-07 23:20:33 +0700

    Update video.php

--- components/video_page/video.php
+++ components/video_page/video.php
@@ -8,7 +8,7 @@
                     <img id="thumb" src="./uploads/thumbs/6418286203.jpg" alt="">
                 </div>
                 <video id="player">
-                    <source src="./uploads/6418286203.mp4" type="video/mp4">
+                    <source src="./uploads/641716.mp4" type="video/mp4">
                 </video>
             </div>
         </div>
