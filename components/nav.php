064bd2863ba437e7310e177116178dda19c87aa6
Minh Kỳ <minhky.book@gmail.com>
2023-04-08 23:34:07 +0700

    Update nav.php

--- components/nav.php
+++ components/nav.php
@@ -1,4 +1,81 @@
 <?php
+    function menuHTML(){
+        ?>
+        <div class="nav__menu-list p-1">                         
+            <ul>
+                <li id="close-menu">
+                    <span class="nav__menu mr-h-5" title="Click to show the menu">
+                        <i class="fa-solid fa-bars"></i>
+                    </span>
+                </li>
+                <li>
+                    <a href="#1234">
+                        <span class="mr-h-5">
+                        <i class="fa-solid fa-house"></i>
+                    </span>
+                    Home 
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="mr-h-5">
+                            <i class="fa-solid fa-video"></i>
+                        </span>
+                        History
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="mr-h-5">
+                            <i class="fa-solid fa-house"></i>
+                        </span>
+                        Your videos
+                    </a>
+                </li>
+                <li id="category-show">
+                    <span class="mr-h-5">
+                        <i class="fa-brands fa-discourse"></i>
+                    </span>
+                    Explore
+                    <span id="category-icon" class="float-right"><i class="fa-solid fa-caret-down"></i></span>
+                </li>
+                <ul id="category-list" class="pl-1 mt-h-5">
+                        <li><a href="">
+                            Music
+                        </a></li>
+                        <li><a href="">
+                            Music
+                        </a></li>
+                        <li><a href="">
+                            Music
+                        </a></li>
+                        <li><a href="">
+                            Music
+                        </a></li>
+                        <li><a href="">
+                            Music
+                        </a></li>
+                </ul>
+                <li>
+                    <a href="">
+                        <span class="mr-h-5">
+                            <i class="fa-solid fa-gear"></i>
+                        </span>
+                        Setting
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="mr-h-5">
+                            <i class="fa-solid fa-right-from-bracket"></i>
+                        </span>
+                        Log out
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <?php
+    }
     function navHTML(){
         ?>
         <nav class="nav sticky-nav py-1">
@@ -9,81 +86,9 @@
                 <div class="nav__logo" title="JQKTube website">
                     <span>Logo website</span>
                 </div>
-                
             </div>
-            <div class="nav__menu-list p-1">                         
-                    <ul>
-                        <li id="close-menu">
-                            <span class="nav__menu mr-h-5" title="Click to show the menu">
-                                <i class="fa-solid fa-bars"></i>
-                            </span>
-                        </li>
-                        <li>
-                            <a href="#1234">
-                                <span class="mr-h-5">
-                                <i class="fa-solid fa-house"></i>
-                            </span>
-                            Home 
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <span class="mr-h-5">
-                                    <i class="fa-solid fa-video"></i>
-                                </span>
-                                History
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <span class="mr-h-5">
-                                    <i class="fa-solid fa-house"></i>
-                                </span>
-                                Your videos
-                            </a>
-                        </li>
-                        <li id="category-show">
-                            <span class="mr-h-5">
-                                <i class="fa-brands fa-discourse"></i>
-                            </span>
-                            Explore
-                            <span id="category-icon" class="float-right"><i class="fa-solid fa-caret-down"></i></span>
-                        </li>
-                        <ul id="category-list" class="pl-1 mt-h-5">
-                                <li><a href="">
-                                    Music
-                                </a></li>
-                                <li><a href="">
-                                    Music
-                                </a></li>
-                                <li><a href="">
-                                    Music
-                                </a></li>
-                                <li><a href="">
-                                    Music
-                                </a></li>
-                                <li><a href="">
-                                    Music
-                                </a></li>
-                        </ul>
-                        <li>
-                            <a href="">
-                                <span class="mr-h-5">
-                                    <i class="fa-solid fa-gear"></i>
-                                </span>
-                                Setting
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <span class="mr-h-5">
-                                    <i class="fa-solid fa-right-from-bracket"></i>
-                                </span>
-                                Log out
-                            </a>
-                        </li>
-                    </ul>
-            </div>
+            <!-- Menu -->
+            <?=menuHTML()?>
             <div class="nav__search">
                 <span id="nav__search-icon" title="Click here to search video"><img src="./assets/icons/search.svg" alt="search icon"></span>
                 <input class="nav_search-input" type="search" placeholder="Search..."/>
