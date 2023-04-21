ee493c394debdb4688ad7e9914c17a5392ec937b
Minh Kỳ <minhky.book@gmail.com>
2023-04-21 14:39:51 +0700

    new config

--- components/scripts.php
+++ components/scripts.php
@@ -2,7 +2,6 @@
     function scriptsHTML(){
         $url = $_SESSION["url"];
         ?>
-        <script src="<?=$url?>/assets/js/videoplayer.js"></script>
         <script src="<?=$url?>/assets/js/view.js" type="module"></script>
         <?php
     }
