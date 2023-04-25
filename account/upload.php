84a885071c0327eccca18168cf1751ac21343584
Minh Kỳ <minhky.book@gmail.com>
2023-04-25 22:00:09 +0700

    new update

--- account/upload.php
+++ account/upload.php
@@ -1,20 +1,210 @@
 <?php
     require_once('../components/layout.php');
+    $url = $_SESSION["url"];
+    // Check login
+    $isLogin = false;
+    if(isset($_SESSION["account"])){
+        $isLogin = true;
+        $account = $_SESSION['account'];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?=head()?>
-    <title>Upload</title>
+    <title>Profile | JQKTube</title>
 </head>
 <body>
     <!-- Navigation -->
     <?=nav()?>
 
     <!-- Container -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="profile__background">
+                <img src="<?=$url?>/assets/imgs/bg-profile.jpg" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="container container__profile container__profile--manage">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="profile__avatar">
+                                <img src="<?=$url?>/api/<?=$account["avatarPath"]?>" class="profile__avatar-img" src="" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="profile__info">
+                                <div class="profile__name">Shigeo Tokuda</div>
+                                <div class="profile__tag">@tokuda.jav</div>
+                                <div class="profile__bio mt-1">Actor</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="profile__filter">
+                                <div class="profile__filter-cate">
+                                    <ul class="profile-filter-list">
+                                        <li id="profile_home" class="profile-filter-item active">Home</li>
+                                        <li id="profile_playlist" class="profile-filter-item">Playlist</li>
+                                        <li id="profile_liked" class="profile-filter-item">Liked videos</li>
+                                        <li id="profile_videos" class="profile-filter-item">Your videos</li>
+                                        <li id="profile_about" class="profile-filter-item">About</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <hr class="mt-1 hr-color">
+            </div>
+        </div>
+        
+    </div>
     <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="wrapper__upload">
+                    <!-- Heading -->
+                    <div class="upload__heading text-center">
+                        <h3 class="py-1">Upload your videos here</h3>
+                        <hr class="hr-color mb-1">
+                    </div>
+                    <!-- Processes -->
+                    <div class="upload__processes">
+                        <div id="step_1" class="processes__step">
+                            <span class="step__bar step__bar--active"></span>
+                            <div class="step__title step__title--active">
+                                <input value="upload__video" type="radio" name="process__bar" id="" checked>
+                                <span>Upload your video</span>
+                            </div>
+                        </div>
+                        <div id="step_2" class="processes__step">
+                            <span class="step__bar"></span>
+                            <div class="step__title">
+                                <input value="info__video" type="radio" name="process__bar" id="">
+                                <span>Video info</span>
+                            </div>
+                        </div>
+                        <div id="step_3" class="processes__step">
+                            <span class="step__bar"></span>
+                            <div class="step__title">
+                                <input value="upload__tbumbnail" type="radio" name="process__bar" id="">
+                                <span>Upload thumbnail</span>
+                            </div>
+                        </div>
+                        <div id="step_4" class="processes__step">
+                            <span class="step__bar"></span>
+                            <div class="step__title">
+                                <input value="add__tags" type="radio" name="process__bar" id="">
+                                <span>Tags and restrictions</span>
+                            </div>
+                        </div>
+                        <div id="step_5" class="processes__step">
+                            <span class="step__bar"></span>
+                            <div class="step__title">
+                                <input value="confirm__upload" type="radio" name="process__bar" id="">
+                                <span>Confirm</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Content -->
+                    <div class="wrapper">
+                        <!-- Select video -->
+                        <div class="upload__content mt-4">
+                            <div class="upload__video">
+                                <img id="upload_input" src="<?=$url?>/assets/imgs/Browse-video-button.png" alt="">
+                                <span class="my-1">Select a video file to upload</span>
+                                <div class="video__preview text-center">
+                                    <span>Preview</span>
+                                    <video class="d-block" id="video__preview-src" controls>
+                                        <source>
+                                    </video>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Update info -->
+                        <div class="upload__info mt-4">
+                            <div class="form-input">
+                                <label>Title <small>(require)</small></label>
+                                <input id="title" type="text" name="title" placeholder="Enter the title of the video" require>
+                                <small class="alert-error"></small>
+                            </div>
+                            <div class="form-input">
+                                <label>Description</label>
+                                <textarea id="desc" type="text" name="desc" placeholder="Enter the description of the video"></textarea>
+                                <small class="alert-error"></small>
+                            </div>                            
+                        </div>
+                        <!-- Upload thumbnail -->
+                        <div class="upload__thumbnail mt-4">
+                            <span>Upload a picture that's the face of your video</span>
+                            <div id="upload_thumb" class="mt-3 thumbnail__block">
+                                <img src="<?=$url?>/assets/icons/Image-edit.png" alt="">
+                                <span>upload thumbnail</span>
+                            </div>
+                            <div class="thumbnail__preview">
+                                <span>Preview</span>
+                                <img>
+                            </div>
+                        </div>
+                        <!-- Add tags and restrictions -->
+                        <div class="upload__stric mt-4">
+                            <div class="form-input">
+                                <label>Tags</label>
+                                <small class="text-fade">
+                                    Adding tags to help your video get recommended to the right audience
+                                </small>
+                                <input id="tags" type="text" name="tags" placeholder="Enter the tags of the video" require>
+                                <small class="alert-error"></small>
+                            </div>
+                            <div class="form-input">
+                                <label for="">Age restriction</label>
+                                <small>This video might not be suitable for children</small>
+                                <div class="input-radio-group">
+                                    <div class="input-group">
+                                        <input id="all-age" type="radio" name="age" value="all-age" checked/>
+                                        <label for="">All ages</label>
+                                    </div>
+                                    <div class="input-group">
+                                        <input id="age-18" type="radio" name="age" value="age-18"/>
+                                        <label for="">R - 18</label>
+                                    </div>
+                                </div>
+                            </div>                        
+                        </div>
+                        <!-- Confirm -->
+                        <div class="upload__confirm mt-4">
+
+                        </div>
+                    </div>
+                    <!-- Unload percents/footer -->
+                    <div class="upload__footer my-3 py-2">
+                        <div class="upload__footer-backward">
+                            <button class="btn btn-primary">BACK</button>
+                        </div>
+                        <div class="upload__percent text-fade">
+                            
+                        </div>
+                        <div class="upload__footer-forward">
+                            <button class="btn btn-primary">NEXT</button>
+                            <button id="btn_confirm" class="btn btn-primary my-1">UPLOAD VIDEO</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- Loading scripts -->
     <?=scripts()?>
+    <script src="<?=$url?>/assets/js/module/uploadvideo.js" type="module"></script>
 </body>
 </html>
---
+++ assets/js/module/uploadvideo.js
@@ -0,0 +1,155 @@
+import {$,$$} from './config.js';
+(()=>{
+    const stepSate = {
+        state: 0,
+        showContent(node){
+            // node.style.display = "block";
+            node.style = "display: block;";
+        },
+        setState(param){
+            if(this.state !== param){
+                if(param === 1){
+                    $('.upload__footer-backward').querySelector('button').style.display = "none";
+                }else{
+                    $('.upload__footer-backward').querySelector('button').style.display = "block";
+                }
+                if(param === 5){
+                    $('.upload__footer-forward').querySelector('button').style.display = "none";
+                    $('#btn_confirm').style.display = "block";
+                }else{
+                    $('#btn_confirm').style.display = "none";
+                    $('.upload__footer-forward').querySelector('button').style.display = "block";
+                }
+                defaultState();
+                this.state = param;
+                switch(this.state){
+                    case 1:{
+                        const step_block = $('#step_1');
+                        activeBar(step_block);
+                        this.showContent($('.upload__content'));
+                    }
+                    break;
+                    case 2:{
+                        const step_block = $('#step_2');
+                        activeBar(step_block);
+                        this.showContent($('.upload__info'));
+                    }
+                    break;
+                    case 3:{
+                        const step_block = $('#step_3');
+                        activeBar(step_block);
+                        this.showContent($('.upload__thumbnail'));
+                    }
+                    break;
+                    case 4:{
+                        const step_block = $('#step_4');
+                        activeBar(step_block);
+                        this.showContent($('.upload__stric'));
+                    }
+                    break;
+                    case 5:{
+                        const step_block = $('#step_5');
+                        activeBar(step_block);
+                        this.showContent($('.upload__confirm'));
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    stepSate.setState(1);
+    const steps = $$('.processes__step');
+    for(let i = 1; i <= steps.length; i++){
+        steps[i-1].onclick = ()=> {
+            stepSate.setState(i);
+        }
+    }
+    
+    // Backward
+    $('.upload__footer-backward').onclick = ()=>{
+        stepSate.setState(stepSate.state - 1);
+    }
+    $('.upload__footer-forward').onclick = ()=>{
+        stepSate.setState(stepSate.state + 1);
+    }
+    // Progress bar
+    function defaultState(){
+        const processSteps = $$('.processes__step');
+        processSteps.forEach(step => {  
+            if(step.querySelector('.step__bar').classList.contains('step__bar--active')){
+                step.querySelector('.step__bar').classList.remove('step__bar--active');
+            }
+            if(step.querySelector('.step__title').classList.contains('step__title--active')){
+                step.querySelector('.step__title').classList.remove('step__title--active')
+            }
+        });
+        $('.upload__content').style.display = "none";
+        $('.upload__info').style.display = "none";
+        $('.upload__thumbnail').style.display = "none";
+        $('.upload__stric').style.display = "none";
+        $('.upload__confirm').style.display = "none";
+    }
+    function activeBar(node){
+        node.querySelector('.step__bar').classList.add('step__bar--active');
+        node.querySelector('.step__title').classList.add('step__title--active');
+        node.querySelector('input').checked = true;
+    }
+
+
+    const selectVideo = $('#upload_input'); //For upload video
+    const selectThumbnail = $('#upload_thumb'); //For upload thumbnail
+    
+
+    const formUpload = {
+        userId: JSON.parse(sessionStorage.profile).userId,
+        video: "",
+        thumbnail: "",
+        title: "",
+        description: "",
+        tags: "",
+        age_restric: "",
+    }
+    // Select file for video
+    selectVideo.onclick=()=>{
+        let input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = ()=>{
+            let files = Array.from(input.files);
+            const tempVideo = URL.createObjectURL(files[0]);
+            $('.video__preview').style.display = "block";
+            $('#video__preview-src').src = tempVideo;
+            formUpload.video = files[0];
+            formUpload.title = files[0]['name'];
+            $('#title').value = formUpload.title;
+        }
+        input.click();
+    }
+
+    // Select file for thumbnail
+    selectThumbnail.onclick=()=>{
+        let input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = ()=>{
+            let files = Array.from(input.files);
+            console.log(files[0]);
+            const preview = $('.thumbnail__preview');
+
+            if(files[0]){
+                preview.style.display = "block";
+                let tempUrl = URL.createObjectURL(files[0]);
+                preview.querySelector('img').src = tempUrl;
+                formUpload.thumbnail = files[0];
+            }
+        }
+        input.click();
+    }
+
+    $('#btn_confirm').onclick =()=>{
+        formUpload.title = $('#title').value;
+        formUpload.description = $('#desc').value;
+        formUpload.tags = $('#tags').value;
+        formUpload.age_restric = $('input[name="age"]:checked').value;
+        console.log(formUpload);
+    }
+
+})();
