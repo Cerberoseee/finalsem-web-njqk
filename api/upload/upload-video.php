e09e0ef93376ea02dc8e96f0c387562a09742f53
Cerberose <tranlethaison123@gmail.com>
2023-05-01 19:10:37 +0700

    update sql video upload

--- api/upload/upload-video.php
+++ api/upload/upload-video.php
@@ -29,13 +29,22 @@
     $videoId = crc32(uniqid());
     $thumbnailPath = processThumbnailUpload($file["thumbnail"], $videoId);
     $videoPath = processVideoUpload($file["video"], $videoId);
+    $tags = isset($_POST["tags"]) ? $_POST["tags"] : "";
+    $views = 0;
+    $uploadTime = date("yyyy-MM-dd hh:mm:ss");
     
     $description = isset($_POST["description"]) ? $_POST["description"] : "";
     $ageRestricted = isset($_POST["age_restric"]) ? $_POST["age_restric"] : "";
 
     //SQL Execution here
 
+    $cm = "INSERT INTO video VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $exec = $dbCon -> prepare($cm);
+    $exec -> bind_param("sssissiisssi", $videoId, $title, $description, $views, $thumbnailPath, $tags, 0, 0, $uploadTime, "active", $videoPath, intval($ageRestricted));
+    $exec -> execute();
+    $result = $exec -> get_result();
 
+    
     //unlink("../../uploads/videos/temp/" . $userId . "/" . basename("video.") . pathinfo($file["video"]["name"], PATHINFO_EXTENSION));
     echo json_encode(array("status" => true, "header" =>
       array("content" => "Upload sucessfully", "id"=> $videoId)
